47098da75b0882ab9c1c6fb888574a55668a7d92
JoseRodriguesF <jose.antonio.220507@gmail.com>
2025-11-13 16:37:03 -0300

    criando estrutura de pastas e configurando projeto

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <Header />
+      <main style={{padding: 16}}>
+        <h2>Bem-vindo ao Telemedicina</h2>
+        <p>Esta é a tela inicial. Edite `src/screens/Home.tsx` para personalizar.</p>
+      </main>
+    </div>
+  );
+}
---
+++ src/lib/api/index.ts
@@ -0,0 +1,2 @@
+export { default as axiosClient } from "./axios";
+export { fetchAPI } from "./fetch";
