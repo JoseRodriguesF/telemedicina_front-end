f1a40b6a7538151563711b89cf2ad4bb5262babf
JoseRodriguesF <jose.antonio.220507@gmail.com>
2026-04-22 19:08:40 -0300

    fix: removed unexpected header from landing page and adjusted hero padding

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,29 +1,7 @@
 "use client";
 
-import Link from 'next/link';
 import './header.css';
 
 export default function Header() {
-  return (
-    <header className="site-header">
-      <div className="container">
-        <div className="header-inner">
-          <div className="header-left">
-            <Link href="/" className="brand">
-              <img src="/images/logo_matriarca.png" alt="Matriarca" width="140" height="40" className="header-logo" />
-            </Link>
-            <nav className="header-nav">
-              <Link href="#servicos">Serviços</Link>
-              <Link href="#entender">Como Funciona</Link>
-              <Link href="#faq">FAQ</Link>
-            </nav>
-          </div>
-          <div className="header-actions">
-            <Link href="/login" className="btn-login">Entrar</Link>
-            <Link href="/register" className="btn-signup">Cadastrar</Link>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+  return null;
 }
